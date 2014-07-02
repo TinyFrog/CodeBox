35d1224195b1d6ada507773bfc5d99a1dbb18cf4
tinyfrog <gogoshaobo@gmail.com>
2014-07-01 22:33:17 -0700

    Update char_pointer.cpp

--- c++/char_pointer.cpp
+++ c++/char_pointer.cpp
@@ -1,19 +1,21 @@
 char* a = "foo bar";
-while(*a != ' '){
+// spaces
+while(*a != ' ') {
     cout<<*a<<endl;
     a++;
 }
-while(*a != '\0'){
+
+// end
+while(*a != '\0') {
     cout<<*a<<endl;
     a++;
 }
 
 char* a = "foo bar";
-for(int i = 0; i<strlen(a);a++){
+for(int i = 0; i<strlen(a); a++) {
     cout<<a[i]<<endl;
 }
 
-// how can we do assignment by char*
 // *pointer can be left-value!!!
 char a[] = "abcd";
 char* b = a;
@@ -22,9 +24,7 @@ b++;
 
 cout<<a<<endl;
 
-
-// this is wrong: 'a' needs to be large enough to hold b, otherwise EXC_BAD_ACCESS
+// this is WRONG: 'b' needs to be large enough to hold a, otherwise EXC_BAD_ACCESS
 char* a = "i eat double cheeseburger today";
 char* b = new char[8];
-
 strcat(a,b);
